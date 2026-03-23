e25552edd6ae336f42db012718c8c926c41d90c4
Scott <scottld@stud.ntnu.no>
2026-03-23 12:45:31 +0100

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../components/HelloWorld.vue'
+
+const routes = [
+  { path: '/', component: Home }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
